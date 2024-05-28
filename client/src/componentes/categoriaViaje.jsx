836b82a22c153333412a4645c04181ef48952977
mauricioLara1 <yhonn.lara@correounivalle.edu.co>
2024-05-27 19:54:27 -0500

    optimizado proceso categoria

--- client/src/componentes/categoriaViaje.jsx
+++ client/src/componentes/categoriaViaje.jsx
@@ -4,27 +4,6 @@ import { PRODUCTS } from './productos'
 
 export default function CategoriaViaje() {
 
-  const productosViaje = async (data) => {
-    try {
-        const url = `http://localhost:5000/productos/viaje`;
-        const response = await fetch(url, {
-            method : "GET",
-            headers: {"Content-Type": "application/json"}
-        });
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error.message);
-    }
-  };
-
-  const mostrarProductosViaje = async () => {
-      const productosMostrar = await productosViaje (); // Esperar a que la promesa se resuelva
-      console.log("return: ", productosMostrar); // Ahora productosMostrar debería contener los datos
-  };
-  
-  mostrarProductosViaje();
-
   useEffect(() =>{
     document.title="Viaje"
   })
